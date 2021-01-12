36377123c0731a2d7ec4c280d9d1323dc7094ed2
liuxiang <zmsnlxx@163.com>
2021-01-12 14:33:41 +0800

    feat: 完成周报/月报改版

--- pages/add-report/add-report.js
+++ pages/add-report/add-report.js
@@ -3,10 +3,64 @@ import { ajax } from '../../utils/http'
 
 Page({
   data: {
-    form: { type: '', title: '', content: '' },
     type: '请选择报表类型',
     show: false,
-    types: [ { text: '周报', value: 1 }, { text: '月报', value: 2 } ]
+    types: [{ text: '周报', value: 1 }, { text: '月报', value: 2 }],
+    params: [],
+    paramsType: '',
+    isDetail: false,
+    detail: {}
+  },
+  onLoad(option) {
+    const { type } = option
+    wx.setNavigationBarTitle({
+      title: type === '1' ? '新增报表' : '报表详情'
+    })
+    if (type === '1') {
+      this.checkParams(1)
+    } else {
+      const { content, type } = wx.getStorageSync('report-detail');
+      this.setData({ isDetail: true, params: content, type: type === 1 ? '周报' : '月报' })
+    }
+  },
+  checkParams(type) {
+    let params = []
+    if (this.data.params.length) {
+      params = this.data.params
+      params.forEach((item,index) => {
+        item.label = index === 0 ? type === 1 ? '本周工作' : '本月工作' : type === 1 ? '下周工作' : '下月工作'
+      })
+    } else {
+      params = [
+        { label: type === 1 ? '本周工作' : '本月工作', next: false, list: [{ title: '', content: '' }], summary: '' },
+        { label: type === 1 ? '下周工作' : '下月工作', next: true, list: [{ title: '', content: '' }], summary: '' },
+      ]
+    }
+    this.setData({ params })
+  },
+  changeSummary(e) {
+    const { index } = e.currentTarget.dataset
+    const params = this.data.params
+    params[index].summary = e.detail.value
+    this.setData({ params })
+  },
+  change(e) {
+    const { index, id, name } = e.currentTarget.dataset
+    const params = this.data.params
+    params[index].list[id][name] = e.detail.value
+    this.setData({ params })
+  },
+  add(e) {
+    const { index } = e.currentTarget.dataset
+    const params = this.data.params
+    params[index].list.push({ title: '', content: '' })
+    this.setData({ params })
+  },
+  clear(e) {
+    const { id, index } = e.currentTarget.dataset
+    const params = this.data.params
+    params[index].list.splice(id, 1)
+    this.setData({ params })
   },
   onChange(e) {
     const model = e.currentTarget.dataset.name
@@ -21,21 +75,28 @@ Page({
   },
   onConfirm(e) {
     const { value, text } = e.detail.value
-    this.setData({ 'form.type': value, type: text, show: false })
+    this.setData({ paramsType: value, type: text, show: false })
+    this.checkParams(Number(value))
   },
   submit() {
-    if (!this.data.form.title) return Toast.fail('请输入标题！')
-    if (!this.data.form.content) return Toast.fail('请输入内容！')
-    if (!this.data.form.type) return Toast.fail('请选择类型！')
+    const [one, two] = this.data.params
+    if (!one.list[0].title || !one.list[0].content) return Toast.fail(`请至少填写一条${one.label}内容`)
+    if (!two.list[0].title || !two.list[0].content) return Toast.fail(`请至少填写一条${two.label}内容`)
+    if (!one.summary) return Toast.fail(`请填写${one.label}工作总结`)
+    if (!two.summary) return Toast.fail(`请填写${two.label.slice(0,2)}需协调工作`)
+    if (!this.data.paramsType) return Toast.fail('请选择类型！')
 
-    ajax('/index/userReport/report', this.data.form, 'post').then(() => {
+    ajax('/index/userReport/report', { type: this.data.paramsType, content: this.data.params }, 'post').then(() => {
       Toast({
         type: 'success',
         message: '提交成功!',
         onClose: () => {
-          wx.navigateTo({ url: '/pages/my-report/my-report' })
+          wx.navigateBack({ delta: 1 })
         },
       });
     })
+  },
+  cancel() {
+    wx.navigateBack({ delta: 1 })
   }
 })
--- pages/my-report/my-report.js
+++ pages/my-report/my-report.js
@@ -4,9 +4,37 @@ Page({
   data: {
     active: 0,
     list: [],
+    form: {
+      startTime: new Date().getTime() + 1000 * 60 * 60 * 24 * -7,
+      endTime: new Date().getTime(),
+    },
+    show: { startTime: false, endTime: false },
+    maxDate: new Date().getTime(),
+    formatter(type, value) {
+      if (type === 'year') {
+        return `${value}年`
+      } else if (type === 'month') {
+        return `${value}月`
+      }
+      return value
+    },
   },
-  onLoad() {
+  onShow() {
     this.getList()
+  },
+  onDateConfirm(e) {
+    const model = e.currentTarget.dataset.name
+    this.setData({ [`form.${model}`]: e.detail, [`show.${model}`]: false })
+    this.getList()
+  },
+  preventTouchMove() {},
+  showClick(e) {
+    const model = e.currentTarget.dataset.name
+    this.setData({ [`show.${model}`]: true })
+  },
+  onCancel(e) {
+    const model = e.currentTarget.dataset.name
+    this.setData({ [`show.${model}`]: false })
   },
   onChange(e) {
     const active = e.detail.name
@@ -14,17 +42,20 @@ Page({
     this.getList()
   },
   getList() {
-    ajax('/index/userReport/list', { type: this.data.active + 1, page: 1, pageSize: 99 }).then(res => {
+    const { startTime, endTime } = this.data.form
+    const result = { startTime: parseInt(startTime / 1000), endTime: parseInt(endTime / 1000) }
+    const params = Object.assign({}, { type: this.data.active + 1, page: 1, pageSize: 99 }, result)
+    ajax('/index/userReport/list', params).then(res => {
       this.setData({ list: res.list })
     })
   },
   addReport() {
-    wx.navigateTo({ url: '/pages/add-report/add-report' })
+    wx.navigateTo({ url: '/pages/add-report/add-report?type=1' })
   },
   goDetail(e) {
     const index = e.currentTarget.dataset.index
     const current = this.data.list[index]
     wx.setStorageSync('report-detail', current);
-    wx.navigateTo({ url: '/pages/report-detail/report-detail' })
+    wx.navigateTo({ url: '/pages/add-report/add-report?type=2' })
   }
 })
