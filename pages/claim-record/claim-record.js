bbfe84e064979f83c343c535201cddf940077430
liuxiang <zmsnlxx@163.com>
2021-01-14 00:46:02 +0800

    fix: 申领添加楼层

--- pages/claim-record/claim-record.js
+++ pages/claim-record/claim-record.js
@@ -4,20 +4,30 @@ import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
 Page({
   data: {
     list: [],
-    user: {}
+    show: false,
+    floors: [],
+    floor: ''
   },
   onLoad() {
-    const user = wx.getStorageSync('user');
-    this.setData({ user })
+    const { floor } = wx.getStorageSync('user');
+    this.setData({ floors: floor.split(',') })
   },
   onShow() {
     ajax('/index/apply/list').then(res => {
       this.setData({ list: res.list })
     })
   },
-  jump() {
-    const floor = this.data.user.floor
+  onConfirm(e) {
+    this.setData({ floor: e.detail.value, show: false, })
+    const floor = this.data.floor
     if (!floor) return Toast({ type: 'fail', message: '暂未设置楼层，请联系管理员' })
     wx.navigateTo({ url: `/pages/warehouse/warehouse?type=2&&floor=${floor}` })
-  }
+  },
+  preventTouchMove() {},
+  showPopup() {
+    this.setData({ show: true })
+  },
+  onCancel() {
+    this.setData({ show: false })
+  },
 })
